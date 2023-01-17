0c1c0e1a24052992b26975254ab5d8e16c40e565
강승권 <kangsg10@naver.com>
2023-01-17 17:48:17 +0900

    Feat: Account Service Interface
    Account 서비스 인터페이스 명세

---
+++ BackEnd/src/account.entity.ts
@@ -0,0 +1,26 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { Role } from './role';
+
+@Entity({ name: 'account' })
+export class Account {
+  @PrimaryGeneratedColumn()
+  account_id: number;
+
+  @Column()
+  @IsNotEmpty()
+  account_user_id: string;
+
+  @Column()
+  @IsNotEmpty()
+  account_user_password: string;
+
+  @Column()
+  @IsNotEmpty()
+  @IsString()
+  account_name: string;
+
+  @Column({ type: 'enum', enum: Role, default: Role.MANAGER })
+  @IsNotEmpty()
+  role: Role;
+}
