9aa4d4a6032d7ad840572e00a5b309143f8aa25e
user188245 <kangsg10@naver.com>
2023-01-17 23:21:35 +0900

    Feat: GameStatistic Service Interface
    GameStatistic 서비스 인터페이스 명세

---
+++ BackEnd/src/game-statistics.service.ts
@@ -0,0 +1,9 @@
+import { GameStatistic } from './game-statistic.entity';
+
+export interface GamesService {
+
+  notifyPlay(account_id: number, game_id: number): Promise<GameStatistic>;
+
+  getAllGameStatistic(account_id: number): Promise<GameStatistic>;
+
+}
