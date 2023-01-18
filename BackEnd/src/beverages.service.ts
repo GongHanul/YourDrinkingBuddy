21b7b15d96e14f4c1e1315c5c7882c594c1047e8
강승권 <kangsg10@naver.com>
2023-01-18 17:37:01 +0900

    Fix: Fix Wrong Interface Definition
    인터페이스 명세 중 빠진 부분 추가

--- BackEnd/src/beverages.service.ts
+++ BackEnd/src/beverages.service.ts
@@ -3,7 +3,7 @@ import { Pagination } from 'nestjs-typeorm-paginate';
 export interface BeveragesService {
   addBeverage(beverage: Beverage): Promise<Beverage>;
   getBeverageByID(beverage_id: number): Promise<Beverage>;
-  getBeverages(pageno: number, pagesize: number, priority?: string): Promise<Pagination<Beverage>>;
+  getBeverages(pageno: number, pagesize: number, sort?: string, query?: string): Promise<Pagination<Beverage>>;
   getAllBeverage(): Promise<Beverage[]>;
   updateBeverage(beverage: Beverage): Promise<Beverage>;
   deleteBeverage(beverage_id: number): Promise<void>;
