07945e1aae828f0f604d1e266860343ed0d5038e
강승권 <kangsg10@naver.com>
2023-01-18 17:19:52 +0900

    Style: Add Missing Space

--- BackEnd/src/devices.repository.ts
+++ BackEnd/src/devices.repository.ts
@@ -2,7 +2,7 @@ import { Repository } from 'typeorm';
 import { Device } from './device.entity';
 
 export class DevicesRepository extends Repository<Device> {
-  async findAllContainsName(name: string, account_id: number): Promise<Device[]>{
+  async findAllContainsName(name: string, account_id: number): Promise<Device[]> {
     return this.createQueryBuilder('device')
       .select(`device_id,device_name,recipe_maker_ip,recipe_maker_port`)
       .where(`device_name LIKE '%:name%' `, { name })
