6840ccb7e73093c3ec9e337d14df489b2357a297
강승권 <kangsg10@naver.com>
2023-01-18 12:30:35 +0900

    Fix: Fix wrong interface name

--- BackEnd/src/game-statistics.service.ts
+++ BackEnd/src/game-statistics.service.ts
@@ -1,6 +1,6 @@
 import { GameStatistic } from './game-statistic.entity';
 
-export interface GamesService {
+export interface GameStatisticsService {
   notifyPlay(account_id: number, game_id: number): Promise<GameStatistic>;
   getAllGameStatistic(account_id: number): Promise<GameStatistic[]>;
 }
