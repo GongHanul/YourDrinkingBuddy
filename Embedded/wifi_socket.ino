c1eb803745680e2ef546d5bd7dc1ab75f387d9fa
공한울 <gonoghanan@naver.com>
2023-01-19 08:42:01 +0900

    Feat: Wifi socket_continuous response
    아두이노 와이파이 모듈, 연속으로 응답하는 코드로 수정

--- Embedded/wifi_socket.ino
+++ Embedded/wifi_socket.ino
@@ -1,9 +1,7 @@
 #include "ESP8266.h"
-
 #include <SoftwareSerial.h> 
-
 #define SSID "MULTI_GUEST_2"
-#define PASSWORD "guest1357"
+#define PASSWORD "guest1357"  
 #define HOST_NAME "70.12.226.153"
 #define HOST_PORT 12345 /* port*/
 #define MY_NAME "PLAYER1"
@@ -11,6 +9,8 @@
 
 SoftwareSerial esp(2, 3); /* RX:D2, TX:D3 */
 ESP8266 wifi(esp, 9600);
+
+bool isConnected = false;
 char usage[]= "2";
 
 void setup(void)
@@ -44,33 +44,69 @@ void setup(void)
 
   Serial.print("setup end\r\n");
 
-  if(wifi.createTCP(HOST_NAME, HOST_PORT)){
-    Serial.print("create tcp ok\r\n");
-    wifi.send(usage, strlen(usage));
-  }
-  else{
-    Serial.print("create tcp err\r\n");
+  if (isConnected == false){
+    while(1){
+      if(wifi.createTCP(HOST_NAME, HOST_PORT)) {
+        Serial.print("create tcp ok\r\n");
+        isConnected = true;
+        wifi.send(usage, strlen(usage));
+        break;
+      } else {
+        Serial.print("create tcp err\r\n");
+      }
+    }
   }
 }
-  bool isConnected = false;
-  
-char sayso[]= "nice";
+
 
 void loop(void)
 {
 
   uint8_t buffer[128] = {0};
   uint32_t len = wifi.recv(buffer, sizeof(buffer), 10000);
-  wifi.send(sayso, strlen(sayso));
+  wifi.send(usage, strlen(usage));
 
   if (len > 0){
     Serial.print("Received:[");
     for (uint32_t i = 0; i < len-1; i++) {
       Serial.print((char)buffer[i]);
     }
-    Serial.print("]\r\n");
-    char cmd = buffer[0];
-    int ledStatus = digitalRead(LED);
-   }
 
+Serial.print("]\r\n");
+char cmd = buffer[0];
+int ledStatus = digitalRead(LED);
+
+
+
+switch (cmd) {
+
+  case '0':
+
+    if (ledStatus == HIGH) {
+      digitalWrite(LED, LOW);
+      sprintf(buffer, "LED is off.\n");
+      wifi.send(buffer, strlen(buffer));
+    }
+    else {
+      sprintf(buffer, "LED is already off.\n");
+      wifi.send(buffer, strlen(buffer));
+    }
+    break;
+
+  case '1':
+    if (ledStatus == LOW) {
+      digitalWrite(LED, HIGH);
+      sprintf(buffer, "LED is on.\n");
+      wifi.send(buffer, strlen(buffer));
+    }
+
+    else {
+      sprintf(buffer, "LED is already on.\n");
+      wifi.send(buffer, strlen(buffer));
+    }
+    break;
+  default:
+    break;
+}
+  }
 }
