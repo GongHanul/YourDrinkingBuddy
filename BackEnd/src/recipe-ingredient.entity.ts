983033e7f0b518fa72104583b66c647759916b03
강승권 <kangsg10@naver.com>
2023-01-19 17:56:32 +0900

    Fix: Add missing @PrimaryColumn() Decorator
    @PrimaryColumn() 빠져서 추가

--- BackEnd/src/recipe-ingredient.entity.ts
+++ BackEnd/src/recipe-ingredient.entity.ts
@@ -1,16 +1,20 @@
 import { Recipe } from './recipe.entity';
-import { Entity, Column, ManyToOne } from 'typeorm';
+import { Entity, Column, ManyToOne, PrimaryColumn } from 'typeorm';
 import { IsNotEmpty, IsPositive } from 'class-validator';
 
 @Entity({ name: 'recipe_ingredient' })
 export class RecipeIngredient {
+  
   @ManyToOne(() => Recipe, (recipe) => recipe.ingredients, {
     onDelete: 'CASCADE',
     onUpdate: 'CASCADE',
   })
   recipe: Recipe;
 
-  @Column()
+  @PrimaryColumn()
+  recipe_id: number;
+
+  @PrimaryColumn()
   beverage_id: number;
 
   @IsNotEmpty()
