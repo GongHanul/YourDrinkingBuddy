108dea5e100dc48adb97db065a13574f3537afa4
강승권 <kangsg10@naver.com>
2023-01-20 11:21:40 +0900

    Feat: Install passport & Setup boilerplate auth module/service

--- BackEnd/src/accounts.module.ts
+++ BackEnd/src/accounts.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Account } from './account.entity';
 import { AccountsController } from './accounts.controller';
 import { AccountsServiceImpl } from './accounts.service.impl';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Account])],
+  imports: [TypeOrmModule.forFeature([Account]), AuthModule],
   controllers: [AccountsController],
   providers: [AccountsServiceImpl],
 })
