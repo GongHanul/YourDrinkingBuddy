8f382c71dd8800603bf69343370bb8aef1a197ce
lkjin1202 <lkjin1202@gmail.com>
2023-02-10 13:46:32 +0900

    Add : Rankingmodal

--- FrontEnd/f_pjt/src/components/Game/Game5.jsx
+++ FrontEnd/f_pjt/src/components/Game/Game5.jsx
@@ -1,23 +1,54 @@
 import styled from "styled-components";
 import { React, useState, useEffect } from 'react';
+import Modal from '@mui/material/Modal';
+import Game1Rank from "../Ranking/Game1Rank";
 
 function Game5() {
   let [Player, setPlayer] = useState([1])
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+
   return (
-  <><TopDiv>
+  <>
+  <Full>
+  <Display>
   { Player.map(function(e, i){
-    return (<Display index={i}>{i}</Display>)
+    return (<PlayerDisplay index={i}>{i}</PlayerDisplay>)
   })}
-  </TopDiv>
+  </Display>
+  <Side>
+  <End onClick={handleOpen}>END</End>
+  <Modal
+    open={open}>
+    <Game1Rank handleClose = {handleClose}/>
+    </Modal>
+  </Side>
+  </Full>
   </>
   )
 }
-  const TopDiv = styled.div`
+const Full = styled.div`
   display : flex;
   flex-wrap: wrap;
-  width : 100%
-  `
-  const Display = styled.div`
+  width : 100vw;
+`
+const Display = styled.div`
+  display : flex;
+  flex-wrap: wrap;
+  width : 100%;
+  height : 85%;
+`
+const Side = styled.div`
+  display : flex;
+  justify-content: space-evenly;
+  padding : 2vh;
+  width : 100%;
+  height : 10%;
+`
+const PlayerDisplay = styled.div`
   display : flex;
   justify-content: center;
   align-items: center ;
@@ -28,5 +59,18 @@ function Game5() {
   flex-direction: column;
   box-shadow: 0 1px 2px #EDD582 , 0 1px 2px #EDD582 inset;
   box-sizing: border-box;
-  `
+`
+const End = styled.div`
+  display : flex;
+  justify-content: center;
+  align-items : center;
+  color : #1966A5;
+  font-size: 3vh;
+  font-family: 'Jua', sans-serif;
+  font-weight : bold;
+  letter-spacing: 0.3vh;
+  &:hover {
+    color: red;
+  }
+`
 export default Game5
